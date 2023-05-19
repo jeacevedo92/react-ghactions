00d92c7aab16162ca715e2bf3d12aebb3869771f
jhonacevedo <jhonacevedo@habi.co>
2023-05-18 22:02:26 -0500

    fix name repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://jeacevedo9.github.io/react-ghactions/',
+  base: 'https://jeacevedo92.github.io/react-ghactions/',
   plugins: [react()],
 })
